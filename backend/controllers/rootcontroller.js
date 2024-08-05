7b5c17223c258f5614008628cded41dc7effc3d9
vishakha952 <vishakhat895@gmail.com>
2024-08-05 11:11:11 +0530

    project initiated

---
+++ backend/controllers/rootcontroller.js
@@ -0,0 +1,3 @@
+const rootcontroller = (req,res)=>{
+    
+}
---
+++ backend/routes/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const colors =require("colors");
+const dotenv =require("dotenv").config();
+const morgan =require("morgan");
+const rootroute = require("./routes/rootroute");
+
+
+const PORT = process.env.PORT || 6000 || 8000;
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("api is working");
+});
+
+app.use("/fashiontrends",rootroute);
+ 
+app.listen(PORT,()=>{
+ console.log(`server is running on https://localhost:${PORT}`.bgBlue.white);
+});
